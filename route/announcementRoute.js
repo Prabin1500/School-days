00e39187f8578e6dcd471312546d9663d715aa1c
riteshg <riteshg@metropolia.fi>
2022-12-01 00:33:51 +0200

    announcement post request working(tested on postman)

--- route/announcementRoute.js
+++ route/announcementRoute.js
@@ -5,5 +5,6 @@ const router = express.Router();
 const announcementController = require('../controller/announceController');
 
 router.get('/announcement',announcementController.getAnnouncement)
+router.post('/announcement',announcementController.addAnnouncement)
 
 module.exports = router;
