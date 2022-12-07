4e89dc844c5ac38f68bfbe85802bda80870b3c56
riteshg <riteshg@metropolia.fi>
2022-12-07 17:05:48 +0200

    Announcement now works at origin

--- app.js
+++ app.js
@@ -2,12 +2,14 @@
 
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const userRoute = require('./route/userRoute');
 const announcementRoute = require('./route/announcementRoute');
 const authRoute = require("./route/authRoute");
 const passport = require('./utils/passport')
 const port = 3000; 
 
+app.use(cors());
 app.use(express.json()); // for parsing application/json
 app.use(passport.initialize());
 
