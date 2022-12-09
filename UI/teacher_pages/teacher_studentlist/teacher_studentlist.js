a6a2961e91f95603f64f68ed4725887954741de8
asmita143 <89544722+asmita143@users.noreply.github.com>
2022-12-09 16:01:48 +0200

    page redirection for teacher_pages

--- UI/teacher_pages/teacher_studentlist/teacher_studentlist.js
+++ UI/teacher_pages/teacher_studentlist/teacher_studentlist.js
@@ -1,4 +1,9 @@
-'use strict';
+"use strict";
+
+
+
+
+ 
 
 const url = 'http://localhost:3000';
 
@@ -10,9 +15,8 @@ const parentlist = document.querySelector('#listparent');
 const studentlist = document.querySelector('#liststudent');
 const btnparent = document.querySelector('#btnparent');
 const topsection = document.querySelector('.topsection');
-const displayparentlist = document.querySelector('.parentlist');
 const displaystudentlist = document.querySelector('.studentlist');
-const btnannouncement = document.querySelector('.active');
+const btnannouncement = document.querySelector('#btnannouncement');
 const homebtn = document.querySelector('#home');
 
 let showhide = false;
@@ -20,94 +24,12 @@ let showhide = false;
 let user = JSON.parse(sessionStorage.getItem('user'));
 welcome.innerHTML ='Welcome ' + user.FIRST_NAME;
 
-document.getElementById('submit').onclick= onclickL();
-document.getElementById('submit1').onclick= onclickR();
 
-function onclickL(){
-  document.getElementById('userssn').value = user.USERSSN;
-};
-
-function onclickR(){
-  document.getElementById('userssn2').value = user.USERSSN;
-};
 
 btnannouncement.addEventListener('click', () => {
-  getAnnouncement();
-  displayparentlist.style.display='none';
-  topsection.style.display='block';
+  location.href="../teacher_announcement/teacher_announcement.html"
 });
 
-btnparent.addEventListener('click', () => {
-  location.href="../teacher_studentlist/teacher_studentlist.html"
-  getAllUsers();
-});
-
-const createAnnouncementCards = (announcements) =>{
-  announcement.innerHTML = '';
-
-  for(let i = 0; i<=announcements.length; i++ ){
-
-    const div1 = document.createElement('div');
-    div1.className='text';
-    const div2 = document.createElement('div');
-    div2.className ='button';
-
-    const newsImage = document.createElement('img');
-      newsImage.src = '../../../uploads/' + announcements[i].media_filename;
-      newsImage.classList.add('resp');
-      const figure = document.createElement('figure').appendChild(newsImage);
-      figure.style.width = '200px';
-      figure.style.height = '200px';
-      figure.style.marginLeft='20px';
-
-    if(announcements[i].media_filename != null){
-      div1.appendChild(figure);
-    }
-
-    const h2 = document.createElement('h3');
-    h2.innerHTML = `${announcements[i].text}`;
-  
-    const p1 = document.createElement('p');
-    p1.innerHTML = `Posted by ${announcements[i].first_name} ${announcements[i].last_name}  at ${announcements[i].dateandtime}`;
-  
-    // modify button
-    const modButton = document.createElement('button');
-    modButton.innerHTML = 'Modify';
-    modButton.addEventListener('click', async() => {
-      location.href =`../../updateAnnouncement.html?id=${announcements[i].announcementid}`;
-    });
-    modButton.classList.add('button');
-  
-    // delete selected 
-    const delButton = document.createElement('button');
-    delButton.innerHTML = 'Delete';
-    delButton.classList.add('button');
-    delButton.addEventListener('click', async () => {
-      const fetchOptions = {
-        method: 'DELETE',
-      };
-        try {
-          const response = await fetch(url + '/announcement/' + announcements[i].announcementid, fetchOptions);
-          const json = await response.json();
-          console.log('delete response', json);
-          getAnnouncement();
-        } catch (e) {
-          console.log(e.message);
-        }
-      });
-  
-      const li = document.createElement('li');
-      li.classList.add('light-border');
-  
-      div1.appendChild(h2);
-      div2.appendChild(p1);
-      div2.appendChild(modButton);
-      div2.appendChild(delButton);
-      li.appendChild(div1);
-      li.appendChild(div2);
-      announcement.appendChild(li);
-    };
-};
 
 // create User cards
 const createParentCards = (users) => {
@@ -239,40 +161,14 @@ const getAllStudents = async() => {
     console.log("Message " + e.message);
   }
 };
+getAllUsers();
 
-//AJAX CALL 
-const getAnnouncement = async() =>{
-  try{
-      const response = await fetch(url + '/announcementFiltered/'+user.USERSSN);
-      const announcements = await response.json();
-      console.log(announcements);
-      createAnnouncementCards(announcements);
-  }catch(e){
-      console.log(e.message);
-  };
-};
-getAnnouncement();
 
-//check if user wants to upload only text or image and text as announcement
-const radioButtons = document.querySelectorAll("input[name='radio']");
-document.getElementById("withoutImage").style.display = "none";
-let selected = () => {
-  let selectedButton = document.querySelector("input[name='radio']:checked").value;
-  console.log(selectedButton);
-  if(selectedButton == "noImage"){
-      document.getElementById("withoutImage").style.display = "flex";
-      document.getElementById("withImage").style.display = "none";
-  }else if(selectedButton == "Image"){
-      document.getElementById("withoutImage").style.display = "none";
-      document.getElementById("withImage").style.display = "flex";
-  }
-}
-radioButtons.forEach(radioButton => {
-  radioButton.addEventListener("change", selected)   
-})
 
 
 
 
 
  
+
+
