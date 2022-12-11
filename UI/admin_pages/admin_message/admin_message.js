2180ae3400ba997098bd76c76cf8140c60fd02c8
asmita143 <89544722+asmita143@users.noreply.github.com>
2022-12-11 23:57:31 +0200

    redirect pages build

--- UI/admin_pages/admin_message/admin_message.js
+++ UI/admin_pages/admin_message/admin_message.js
@@ -5,12 +5,12 @@ const url = 'http://localhost:3000';
 const btnparent = document.querySelector('#btnparent');
 const btnmessage = document.querySelector('#btnmessage')
 
-btnparent.addEventListener('click', () => {
+btnlist.addEventListener('click', () => {
     location.href="../admin_userlist/admin_userlist.html"
     getAllUsers();
   });
 
-  btnmessage.addEventListener('click', () => {
-    location.href="../admin_message/admin_message.html"
+  btnannouncement.addEventListener('click', () => {
+    location.href="../admin_announcement/admin_announcement.html"
     getAllUsers();
   });
