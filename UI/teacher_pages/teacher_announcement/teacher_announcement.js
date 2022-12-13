03e9d152ed0811573bb4f3a03a39b9aec3f84a2e
asmita143 <89544722+asmita143@users.noreply.github.com>
2022-12-14 00:12:13 +0200

    mobile responsive work

--- UI/teacher_pages/teacher_announcement/teacher_announcement.js
+++ UI/teacher_pages/teacher_announcement/teacher_announcement.js
@@ -60,9 +60,9 @@ const createAnnouncementCards = (announcements) =>{
       newsImage.src = '../../../uploads/' + announcements[i].media_filename;
       newsImage.classList.add('resp');
       const figure = document.createElement('figure').appendChild(newsImage);
-      figure.style.width = '200px';
-      figure.style.height = '200px';
-      figure.style.marginLeft='20px';
+      figure.style.maxWidth = '50%';
+      figure.style.height = 'auto';
+      figure.style.borderRadius='20px 20px 20px 20px'
 
     if(announcements[i].media_filename != null){
       div1.appendChild(figure);
