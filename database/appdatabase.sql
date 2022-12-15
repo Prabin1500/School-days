cd928f3d539ebd1a2114e37002d788e19efc2c5b
riteshg <ritesh.ghimire@metropolia.fi>
2022-12-15 06:27:46 +0000

    Update database/appdatabase.sql

--- database/appdatabase.sql
+++ database/appdatabase.sql
@@ -48,3 +48,28 @@ CREATE TABLE students (
   PRIMARY KEY (childssn),
   FOREIGN KEY (userssn) REFERENCES users(userssn)
 ) ;
+
+
+/*SQL script with all the queries your backend makes.*/
+
+SELECT announcement.announcementid,announcement.text,announcement.media_filename, announcement.dateandtime, users.first_name, users.last_name from announcement,users where announcement.userssn = users.userssn and users.role = "admin" order by announcementid desc;
+SELECT * FROM announcement where announcementid =?;
+SELECT announcement.announcementid,announcement.text,announcement.media_filename, announcement.dateandtime, users.first_name, users.last_name from announcement,users where announcement.userssn=users.userssn and announcement.userssn = ? order by announcementid desc; 
+SELECT announcement.announcementid,announcement.text,announcement.media_filename, announcement.dateandtime, users.first_name, users.last_name from announcement,users where announcement.class=? and announcement.userssn = users.userssn  order by announcementid desc;
+select announcement.announcementid,announcement.text,announcement.media_filename, announcement.dateandtime, users.first_name, users.last_name from announcement,users where announcement.userssn = users.userssn  order by announcementid desc;
+INSERT INTO announcement VALUES (null,?,?,?,?,?);
+INSERT INTO announcement(text,dateandtime,userssn,class) VALUES (?,?,?,?);
+DELETE FROM announcement where announcementid = ?;
+UPDATE announcement SET text =?, dateandtime=? where announcementid = ?;
+select message.description,message.sender,message.dateandtime from message where (reciever=? or sender=?) and (reciever=? or sender=?);
+select message.description,message.sender,message.dateandtime from message where reciever=? order by id asc;
+INSERT INTO message VALUES(null,?,?,?,?);
+select users.first_name,users.last_name,users.email,users.phone_number, users.class, users.role, users.userssn,users.username from users:
+select users.first_name,users.last_name,users.email,users.phone_number, users.class,users.username from users where role = ? or role=?;
+select students.first_name, students.last_name, students.class, students.userssn from students;
+INSERT INTO users(USERSSN, FIRST_NAME, LAST_NAME, EMAIL, PHONE_NUMBER, ROLE, USERNAME, PASSWORD) VALUES (?,?,?,?,?,?,?,?)
+                            [user.userssn, user.firstName, user.lastName, user.email, user.phoneNumber, user.role, user.username, user.password, null];
+INSERT INTO users(USERSSN, FIRST_NAME, LAST_NAME, EMAIL, PHONE_NUMBER, ROLE, USERNAME, PASSWORD, CLASS) VALUES (?,?,?,?,?,?,?,?,?);					
+INSERT INTO students(CHILDSSN, FIRST_NAME, LAST_NAME, CLASS, USERSSN) VALUES (?,?,?,?,?);					
+SELECT * FROM users WHERE users.email = ? or users.username =?;
+select users.first_name,users.last_name,users.username,students.first_name as student_fname,students.last_name as student_lname from users,students where users.userssn = students.userssn;
